284837ce4c915d40791c0378d7d835307fbb9716
Camille <ferreiracamille819@gmail.com>
2025-03-13 22:05:48 -0300

    Add files via upload

---
+++ Emprestimo de livros/src/emprestimo/de/livros/EmprestimoDeLivros.java
@@ -0,0 +1,15 @@
+
+package emprestimo.de.livros;
+
+public class EmprestimoDeLivros {
+
+    public static void main(String[] args) {
+       Livro livro = new Livro("Verity", "Colleen Hoover");
+        System.out.println("Livro: " + livro.getTitulo() + ", Disponível: " + livro.isDisponivel());
+        livro.emprestar();
+        System.out.println("Após empréstimo: " + livro.isDisponivel());
+        livro.devolver();
+        System.out.println("Após devolução: " + livro.isDisponivel());
+    }
+}
+    
